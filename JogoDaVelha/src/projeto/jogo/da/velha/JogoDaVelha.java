c57c6a7649185b8dd5123d93e2f576f147e7e23e
Laís Zanardi <89917698+zanardiL@users.noreply.github.com>
2022-10-04 15:37:55 -0300

    alteração do layout do tabuleiro; atualização do método rodarJogo (antigo jogadas); verificação de empate (após 9 rodadas);

--- JogoDaVelha/src/projeto/jogo/da/velha/JogoDaVelha.java
+++ JogoDaVelha/src/projeto/jogo/da/velha/JogoDaVelha.java
@@ -1,198 +1,214 @@
 package projeto.jogo.da.velha;
-
-import java.util.Arrays;
-import java.util.InputMismatchException;
 import java.util.Scanner;
-
 public class JogoDaVelha {
 
     static int i;
     static int j;
     static Scanner sc = new Scanner(System.in);
-    final static String player1 = "\t X";
-    final static String player2 = "\t O";
-    String nomeUm;
-    String nomeDois;
+    final static String PLAYER_1 = "\tX";
+    final static String PLAYER_2 = "\tO";
 
     public static void main(String[] args) {
+        String nomePlayer1 = "";
+        String nomePlayer2 = "";
 
-        System.out.println();
         saudacao();
-
         String[][] jogo = new String[5][5];
-        int opcao;
         menu();
 
-        try {
-            do {
-                opcao = sc.nextInt();
+        int opcao = sc.nextInt();
 
-                switch (opcao) {
-                    case 1:
-                        tabuleiroRegras();
-
-                    case 2:
-                        System.out.print("Qual é o nome do primeiro jogador? ");
-                        String nomeUm = sc.next();
-
-                        System.out.print("Qual é o nome do segundo jogador? ");
-                        String nomeDois = sc.next();
-
-                        break;
-                    case 3:
-                        System.out.print("Até Logo!");
-
-                        break;
-                    default:
-                        System.out.print("Opção inválida, digite novamente:\n");
-                }
-
-            } while (opcao != 1 || opcao != 2 || opcao != 3);
-        } catch (InputMismatchException exception) {
-            System.out.print("Caractere não reconhecido");
-            return;
+        switch (opcao) {
+            case 1:
+                tabuleiroRegras();
+            case 2:
+                rodarJogo(jogo, nomePlayer1, nomePlayer2);
+                break;
+            case 3:
+                System.out.print("Até Logo!");
+                break;
         }
-
-
-        tabuleiro(jogo);
-        printTabuleiro(jogo);
-        jogadas(jogo, player1, player2);
-
     }
 
-    public static void tabuleiro(String[][] jogo) {   //construção do tabuleiro
-        for (int i = 0; i < jogo.length; i++) {
-            for (int j = 0; j < jogo.length; j++) {
-
-                if (j == 1 || j == 3) {
+    public static void tabuleiro(String[][] jogo){   //construção do tabuleiro
+        for (i = 0; i < jogo.length; i++) {
+            for (j = 0; j < jogo.length; j++) {
+                if ((j == 1 || j==3) && i%2 == 0) {
                     jogo[i][j] = "\t|";
-                } else if (i == 1 || i == 3) {
-                    jogo[i][j] = "\t---";
-                } else jogo[i][j] = "";
+                } else if((i == 1 || i ==3)&& j==0) {
+                    jogo[i][j] = "\t--";
+                } else if(i == 1 || i ==3) {
+                    jogo[i][j] = "----";
+                } else jogo[i][j] = "\t";
             }
         }
-
     }
 
     public static void printTabuleiro(String[][] jogo) { //imprimir tabuleiro
-        for (int i = 0; i < jogo.length; i++) {
-            for (int j = 0; j < jogo.length; j++) {
+        for (i = 0; i < jogo.length; i++) {
+            for (j = 0; j < jogo.length; j++) {
                 System.out.print(jogo[i][j]);
             }
             System.out.println();
         }
     }
-
     public static boolean verificaVencedor(String[][] jogo) {
-        if (jogo[0][0].equals(jogo[0][2]) && jogo[0][0].equals(jogo[0][4])) //A=B=C
+        if(jogo[0][0].equals(jogo[0][2]) && jogo[0][0].equals(jogo[0][4]) && !jogo[0][0].equals("\t")) //A=B=C e !=vazio
             return true;
-        else if (jogo[2][0].equals(jogo[2][2]) && jogo[2][0].equals(jogo[2][4])) //D=E=F
+        else if(jogo[2][0].equals(jogo[2][2]) && jogo[2][0].equals(jogo[2][4]) && !jogo[2][0].equals("\t")) //D=E=F e !=vazio
             return true;
-        else if (jogo[4][0].equals(jogo[4][2]) && jogo[4][0].equals(jogo[4][4])) //G=H=I
+        else if(jogo[4][0].equals(jogo[4][2]) && jogo[4][0].equals(jogo[4][4]) && !jogo[4][0].equals("\t")) //G=H=I e !=vazio
             return true;
-        else if (jogo[0][0].equals(jogo[2][0]) && jogo[0][0].equals(jogo[4][0])) //A=D=G
+        else if(jogo[0][0].equals(jogo[2][0]) && jogo[0][0].equals(jogo[4][0]) && !jogo[0][0].equals("\t")) //A=D=G e !=vazio
             return true;
-        else if (jogo[0][2].equals(jogo[2][2]) && jogo[0][2].equals(jogo[4][2])) //B=E=H
+        else if(jogo[0][2].equals(jogo[2][2]) && jogo[0][2].equals(jogo[4][2]) && !jogo[0][2].equals("\t")) //B=E=H e !=vazio
             return true;
-        else if (jogo[0][4].equals(jogo[2][4]) && jogo[0][4].equals(jogo[4][4])) //C=F=I
+        else if(jogo[0][4].equals(jogo[2][4]) && jogo[0][4].equals(jogo[4][4]) && !jogo[0][4].equals("\t")) //C=F=I e !=vazio
             return true;
-        else if (jogo[0][0].equals(jogo[2][2]) && jogo[0][0].equals(jogo[4][4])) //A=E=I
+        else if(jogo[0][0].equals(jogo[2][2]) && jogo[0][0].equals(jogo[4][4]) && !jogo[0][0].equals("\t")) //A=E=I e !=vazio
             return true;
-        else if (jogo[0][4].equals(jogo[2][2]) && jogo[0][4].equals(jogo[4][0])) //C=E=G
+        else if(jogo[0][4].equals(jogo[2][2]) && jogo[0][4].equals(jogo[4][0]) && !jogo[0][4].equals("\t")) //C=E=G e !=vazio
             return true;
         else
             return false;
     }
-
-    //    public static String[] posicoesDisponiveis(String[] posicoes, String coordenada) {
-//        for (i = 0; i < posicoesDisponiveis.length; i++) {
-//            if (posicoesDisponiveis[i].equals(coordenada))
-//                posicoesDisponiveis[i] = "";
-//        }
-//        return posicoesDisponiveis;
-    // }
-    public static void jogadas(String[][] jogo, String player1, String player2) {
+    public static void rodarJogo(String[][] jogo, String nomePlayer1, String nomePlayer2) {
+        if(nomePlayer1.equals("") && nomePlayer2.equals("")) {
+            System.out.print("Qual é o nome do primeiro jogador? ");
+            nomePlayer1 = sc.next();
+            System.out.print("Qual é o nome do segundo jogador? ");
+            nomePlayer2 = sc.next();
+        }
         int numRodada = 1;
-        String vez = player1;
-        String[] posicoesDisponiveis = {"A", "B", "C", "D", "E", "F", "G", "H", "I"};
+        String vez = PLAYER_1;
+        String vezJogador = nomePlayer1;
+
+        tabuleiro(jogo);
         do {
-            System.out.println("\n Vez da rodada: " + vez);
+            System.out.println("\n Vez do jogador: " + vezJogador);
             System.out.println("ESCOLHA A POSIÇÃO");
             String coordenada = sc.next();
             coordenada = coordenada.toUpperCase();
 
-            for (i = 0; i < posicoesDisponiveis.length; i++) {
-                if (posicoesDisponiveis.equals(coordenada)) {
-                    posicoesDisponiveis[i] = "";
+            if (verificaVazio(jogo, coordenada)) {
+                switch (coordenada) {
+                    case "A":
+                        jogo[0][0] = vez;
+                        break;
+                    case "B":
+                        jogo[0][2] = vez;
+                        break;
+                    case "C":
+                        jogo[0][4] = vez;
+                        break;
+                    case "D":
+                        jogo[2][0] = vez;
+                        break;
+                    case "E":
+                        jogo[2][2] = vez;
+                        break;
+                    case "F":
+                        jogo[2][4] = vez;
+                        break;
+                    case "G":
+                        jogo[4][0] = vez;
+                        break;
+                    case "H":
+                        jogo[4][2] = vez;
+                        break;
+                    case "I":
+                        jogo[4][4] = vez;
+                        break;
+                    default:
+                        System.out.println("Posição inválida!");
+                        System.out.println("Por favor, selecione uma das posições disponíveis: ");
+                }
+                if (numRodada % 2 != 0) {
+                    vez = PLAYER_2;
+                    vezJogador = nomePlayer2;
+                } else {
+                    vez = PLAYER_1;
+                    vezJogador = nomePlayer1;
+                }
+                numRodada +=1;
+                printTabuleiro(jogo);
+
+                if(numRodada>9) { //EMPATE
+                    System.out.println(nomePlayer1 + " e " + nomePlayer2 + " empataram!\n" +
+                            "REVANCHE? ");
+                    System.out.println("\t 1 -SIM\n" +
+                            "\t 2 -NÃO");
+                    int revanche = sc.nextInt();
+                    if(revanche == 1)
+                        rodarJogo(jogo, nomePlayer1, nomePlayer2);
+                    else {
+                        System.out.println("Até logo!");
+                        System.exit(0);
+                    }
                 }
             }
-
-            switch (coordenada) {
-                case "A":
-                    if (jogo[0][0].isBlank())
-                        jogo[0][0] = vez;
-                    else System.out.println(Arrays.toString(posicoesDisponiveis));
-                    printTabuleiro(jogo);
-                    break;
-                case "B":
-                    jogo[0][2] = vez;
-                    printTabuleiro(jogo);
-                    break;
-                case "C":
-                    jogo[0][4] = vez;
-                    printTabuleiro(jogo);
-                    break;
-                case "D":
-                    jogo[2][0] = vez;
-                    printTabuleiro(jogo);
-                    break;
-                case "E":
-                    jogo[2][2] = vez;
-                    printTabuleiro(jogo);
-                    break;
-                case "F":
-                    jogo[2][4] = vez;
-                    printTabuleiro(jogo);
-                    break;
-                case "G":
-                    jogo[4][0] = vez;
-                    printTabuleiro(jogo);
-                    break;
-                case "H":
-                    jogo[4][2] = vez;
-                    printTabuleiro(jogo);
-                    break;
-                case "I":
-                    jogo[4][4] = vez;
-                    printTabuleiro(jogo);
-                    break;
-                default:
-                    System.out.println("Posição inválida!");
-                    //inserir posições disponíveis
-            }
-            if (vez.equals(player1)) {
-                vez = player2;
-            } else {
-                vez = player1;
-            }
-        } while (verificaVencedor(jogo));
+        }
+        while (verificaVencedor(jogo)==false) ;
+    }
+    public static boolean verificaVazio(String[][] jogo, String coordenada) {
+        boolean vazio = false;
+        switch (coordenada) {
+            case "A":
+                if (jogo[0][0].equals("\t"))
+                    vazio = true;
+                break;
+            case "B":
+                if (jogo[0][2].equals("\t"))
+                    vazio = true;
+                break;
+            case "C":
+                if (jogo[0][4].equals("\t"))
+                    vazio = true;
+                break;
+            case "D":
+                if (jogo[2][0].equals("\t"))
+                    vazio = true;
+                break;
+            case "E":
+                if (jogo[2][2].equals("\t"))
+                    vazio = true;
+                break;
+            case "F":
+                if (jogo[2][4].equals("\t"))
+                    vazio = true;
+                break;
+            case "G":
+                if (jogo[4][0].equals("\t"))
+                    vazio = true;
+                break;
+            case "H":
+                if (jogo[4][2].equals("\t"))
+                    vazio = true;
+                break;
+            case "I":
+                if (jogo[4][4].equals("\t"))
+                    vazio = true;
+                break;
+            default:
+                vazio = false;
+                break;
+        }
+        return vazio;
     }
 
 
-    private static void saudacao() {
+
+    private static void saudacao () {
         System.out.println("*** Bem vindo ao jogo da velha. Divirta-se!***");
     }
-
     private static void menu() {
-        System.out.println();
-        System.out.println("Você gostaria de iniciar uma partida? \n" +
+        System.out.print("Você gostaria de iniciar uma partida? \n" +
                 "Digite 1 para regras; \n" +
                 "Digite 2 para iniciar o jogo;\n" +
                 "digite 3 para sair.");
 
 
     }
-
     private static void tabuleiroRegras() {
 
 
@@ -212,6 +228,7 @@ public class JogoDaVelha {
         }
         System.out.println("Na sua vez de jogar, você deve digitar qual letra corresponde ao espaço que você quer ocupar.\n");
     }
+
 
 }
 
